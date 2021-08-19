7620c0a4ecc78a2f48a93cb228614d5e5ef9dd16
adityacaturputra <adityacaturputra25@gmail.com>
2021-08-19 09:35:46 +0700

    CRUD Category Update and Delete

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -16,6 +16,19 @@ module.exports = {
         await Category.create({ name })
         res.redirect('/admin/category')
     },
+    editCategory: async (req, res) => {
+        const { id, name } = req.body;
+        const category = await Category.findOne({ _id: id })
+        category.name = name
+        await category.save()
+        res.redirect('/admin/category')
+    },
+    deleteCategory: async (req, res) => {
+        const { id } = req.params;
+        const category = await Category.findOne({ _id: id })
+        await category.remove()
+        res.redirect('/admin/category')
+    },
 
     viewBank: (req, res) => {
         res.render('admin/bank/view_bank')
