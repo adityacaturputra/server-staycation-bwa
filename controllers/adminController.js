02a22d500f53f5d7b0658a60fecc3008db084cf2
adityacaturputra <adityacaturputra25@gmail.com>
2021-08-20 07:54:50 +0700

    CRUD Bank Update

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,7 @@
 const Category = require('../models/Category');
 const Bank = require('../models/Bank');
+const fs = require('fs-extra');
+const path = require('path')
 
 
 module.exports = {
@@ -92,6 +94,34 @@ module.exports = {
             res.redirect('/admin/bank')
         }
     },
+    editBank: async (req, res) => {
+        try {
+            const { id, name, nameBank, nomorRekening } = req.body
+            const bank = await Bank.findOne({ _id: id })
+            if (!req.file) {
+                bank.name = name
+                bank.nameBank = nameBank
+                bank.nomorRekening = nomorRekening
+                await bank.save();
+            } else {
+                await fs.unlink(path.join(`public/${bank.imageUrl}`))
+                bank.name = name
+                bank.nameBank = nameBank
+                bank.nomorRekening = nomorRekening
+                bank.imageUrl = `images/${req.file.filename}`
+                await bank.save();
+            }
+            req.flash('alertMessage', 'Success update bank')
+            req.flash('alertStatus', 'success')
+            res.redirect('/admin/bank')
+        } catch (error) {
+            req.flash('alertMessage', `Failed update bank: ${error.message}`)
+            req.flash('alertStatus', 'danger')
+            console.log(error)
+            res.redirect('/admin/bank')
+        }
+    },
+
 
     viewItem: (req, res) => {
         res.render('admin/item/view_item', { title: "Staycation | Item" })
