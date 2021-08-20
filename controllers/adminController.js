d93dcbd4419f95ae42b04fdb677d2bb904bae0e0
adityacaturputra <adityacaturputra25@gmail.com>
2021-08-20 18:35:41 +0700

    CRUD Create and Read Item

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,11 @@
 const Category = require('../models/Category');
 const Bank = require('../models/Bank');
+const Item = require('../models/Item');
+const Image = require('../models/Image');
+const mongoose = require('mongoose');
 const fs = require('fs-extra');
 const path = require('path')
+const ObjectId = mongoose.Types.ObjectId
 
 
 module.exports = {
@@ -137,10 +141,54 @@ module.exports = {
             res.redirect('/admin/bank')
         }
     },
-
-
-    viewItem: (req, res) => {
-        res.render('admin/item/view_item', { title: "Staycation | Item" })
+    viewItem: async (req, res) => {
+        try {
+            const item = await Item.find()
+                .populate({ path: 'imageId', select: 'id imageUrl' })
+                .populate({ path: 'categoryId', select: 'id name' })
+            const category = await Category.find()
+            const alertMessage = req.flash('alertMessage');
+            const alertStatus = req.flash('alertStatus');
+            const alert = { message: alertMessage, status: alertStatus }
+            res.render('admin/item/view_item', { title: "Staycation | Item", category, item, alert })
+        } catch (error) {
+            req.flash('alertMessage', `Failed view: ${error.message}`)
+            req.flash('alertStatus', 'danger')
+            console.log(error)
+            res.redirect('/admin/item')
+        }
+    },
+    addItem: async (req, res) => {
+        try {
+            const { categoryId, title, price, city, about } = req.body
+            if (req.files.length > 0) {
+                const category = await Category.findById(categoryId)
+                console.log(category)
+                const newItem = {
+                    categoryId: category._id,
+                    title,
+                    description: about,
+                    price,
+                    city
+                }
+                const item = await Item.create(newItem)
+                category.itemId.push({ _id: item._id })
+                await category.save();
+                for (let i = 0; i < req.files.length; i++) {
+                    const imageSave = await Image.create({ imageUrl: `images/${req.files[i].filename}` })
+                    item.imageId.push({ _id: imageSave._id })
+                }
+                item.save()
+                req.flash('alertMessage', 'Success add item')
+                req.flash('alertStatus', 'success')
+                res.redirect('/admin/item')
+            }
+        } catch (error) {
+            console.log(error)
+            req.flash('alertMessage', `Failed add item: ${error.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/admin/item')
+        }
     },
     viewBooking: (req, res) => {
         res.render('admin/booking/view_booking', { title: "Staycation | Booking" })
