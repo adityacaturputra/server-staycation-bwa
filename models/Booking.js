21f3a697a7b143458d69e897a07c086e2704aafb
adityacaturputra <adityacaturputra25@gmail.com>
2021-08-26 17:15:25 +0700

    Schema Users and seeder

--- models/Booking.js
+++ models/Booking.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const { ObjectId } = mongoose.Schema
 
 const bookingSchema = new mongoose.Schema({
     bookingStartDate: {
@@ -9,48 +10,58 @@ const bookingSchema = new mongoose.Schema({
         type: Date,
         required: true
     },
-    itemId: [{
+    invoice: {
+        type: String,
+        required: true
+    },
+    itemId: {
         _id: {
             type: ObjectId,
             ref: 'Item',
+            required: true
+        },
+        title: {
+            type: String,
             required: true
         },
         price: {
             type: Number,
             required: true
         },
-        night: {
+        duration: {
             type: Number,
             required: true
         }
-    }],
-    memberId: [{
+    },
+    total: {
+        type: Number,
+        required: true
+    },
+    memberId: {
         type: ObjectId,
         ref: 'Member'
-    }],
-    bankId: [{
+    },
+    bankId: {
         type: ObjectId,
         ref: 'Bank'
-    }],
-    proofPayment: {
-        type: String,
-        required: true
     },
-    bankFrom: {
-        type: String,
-        required: true
-    },
-    accountHolder: {
-        type: String,
-        required: true
-    },
-    imageUrl: {
-        type: String,
-        required: true
-    },
-    status: {
-        type: String,
-        required: true
+    payments: {
+        proofPayment: {
+            type: String,
+            required: true
+        },
+        bankFrom: {
+            type: String,
+            required: true
+        },
+        accountHolder: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            required: true
+        }
     }
 });
 
