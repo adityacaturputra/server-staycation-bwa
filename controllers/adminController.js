c9c48389410269bc4a1ec8c77532d7066f9dd55b
adityacaturputra <adityacaturputra25@gmail.com>
2021-08-26 10:07:45 +0700

    Delete Feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -350,6 +350,29 @@ module.exports = {
             res.redirect(`/admin/item/show-detail-item/${itemId}`)
         }
     },
+    deleteFeature: async (req, res) => {
+        const { id, itemId } = req.params
+        try {
+            const feature = await Feature.findOne({ _id: id })
+            const item = await Item.findOne({_id: itemId}).populate('featureId')
+            for (let i = 0; i < item.featureId.length; i++) {
+                if (item.featureId[i]._id.toString() === feature._id.toString()){
+                    item.featureId.pull({_id: feature._id})
+                    await item.save()
+                }
+            }
+            await fs.unlink(path.join(`public/${feature.imageUrl}`))
+            await feature.remove()
+            req.flash('alertMessage', 'Success delete feature')
+            req.flash('alertStatus', 'success')
+            res.redirect(`/admin/item/show-detail-item/${itemId}`)
+        } catch (error) {
+            req.flash('alertMessage', `Failed delete feature: ${error.message}`)
+            req.flash('alertStatus', 'danger')
+            console.log(error)
+            res.redirect(`/admin/item/show-detail-item/${itemId}`)
+        }
+    },
 
     viewBooking: (req, res) => {
         res.render('admin/booking/view_booking', { title: "Staycation | Booking" })
