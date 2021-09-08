a0dba6d86365dead45e6959f01026f0ac895e24a
adityacaturputra <adityacaturputra25@gmail.com>
2021-09-08 20:55:49 +0700

    deploy on heroku and connect to mongodb atlas

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const flash = require('connect-flash');
 
 // import mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/db_staycation', {
+mongoose.connect('mongodb+srv://adityacaturputra:sqkAmfiERitQGuCT@cluster0.reduu.mongodb.net/db_staycation?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
