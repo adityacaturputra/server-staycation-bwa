405199b3ec749105bfd954888d09be48d05803d9
adityacaturputra <adityacaturputra25@gmail.com>
2021-09-10 07:52:17 +0700

    use cors

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ var apiRouter = require('./routes/api');
 
 var app = express();
 
+app.use(cors())
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
